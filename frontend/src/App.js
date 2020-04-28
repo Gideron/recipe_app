b67ce0618197571d8fc6ce2126f7c0e70fdbdb63
Gideron <nico.alakonni@gmail.com>
2020-04-28 23:26:49 +0300

    Moved RecipeListApp to jsx and fixing rating element

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,9 @@ import logo from './logo.svg';
 import MenuIcon from '@material-ui/icons/Menu';
 import PersonIcon from '@material-ui/icons/Person';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import BookmarkIcon from '@material-ui/icons/Bookmark';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import StarIcon from '@material-ui/icons/Star';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
 
 import './App.css';
+import RecipeListApp from './RecipeListApp.jsx';
 
 //mockup data
 import RecipeData from './mockup_data/RecipeList.json'
@@ -26,95 +22,6 @@ class NavigationBar extends React.Component {
     );
   }
 }
-
-class RecipeListApp extends React.Component {
-  render() {
-    return (
-      <div id="content">
-        <h1>Recipes/Bookmarks</h1>
-        <RecipeList />
-      </div>
-    );
-  }
-}
-
-class RecipeList extends React.Component {
-  render() {
-    return (
-      <ul class="recipe-list">
-        {RecipeData.map(recipe => (
-          <li class="recipe-element" key={recipe.id}>
-            <div class="recipe-header">
-                <h2>{recipe.Title}</h2>
-                <a class="bookmark-button"><BookmarkIcon /></a>
-            </div>
-            <RateElement rates={recipe.Rates}/>
-            <img class="recipe-img" src={recipe.Picture} alt="picture of food" />
-          </li>
-        ))}
-      </ul>
-    );
-  }
-}
-
-//TODO: add key to icons
-class RateElement extends React.Component {
-  constructor(props) {
-    super(props);
-    this.currentRate = 0;
-  }
-  showRate() {
-    this.currentRate = 0;
-    console.log(this.props.rates)
-    for(var i = 0; i < this.props.rates.length; i++) {
-      this.currentRate += this.props.rates[i].Rate;
-      if(this.props.rates[i].User == 'ThisUser'){
-        return (
-          <a>
-            <span>
-              _.times({this.props.rates[i].User.Rate}, (y) => {
-                <StarIcon />
-              });
-            </span>
-            _.times({5 - this.props.rates[i].User.Rate}, (y) => {
-                <StarBorderIcon />
-              });
-          </a>
-        );
-      }
-    }
-    console.log(this.currentRate)
-    this.currentRate /= this.props.rates.length;
-    console.log(this.currentRate)
-    console.log(Math.round(this.currentRate))
-    return (
-      <a>
-        _.times({Math.round(this.currentRate)}, (y) => {
-          <StarIcon />
-        });
-        _.times({5 - Math.round(this.currentRate)}, (y) => {
-          <StarBorderIcon />
-        });
-      </a>
-    );
-  }
-
-  /* _(Math.round(this.currentRate)).times(idx => <StarIcon />);*/
-  render() {
-    return (
-      <div class="rate">
-        {this.showRate()}
-      </div>
-    );
-  }
-}
-
-
-
-
-
-
-
 
 function App() {
   return (
---
+++ frontend/src/RecipeListApp.jsx
@@ -0,0 +1,92 @@
+import React from 'react';
+//material icons
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+//mockup data
+import RecipeData from './mockup_data/RecipeList.json'
+
+const RecipeListApp = () => {
+    return (
+    <div id="content">
+        <h1>Recipes/Bookmarks</h1>
+        <RecipeList />
+    </div>
+    );
+}
+
+const RecipeList = () => {
+    return (
+    <ul class="recipe-list">
+        {RecipeData.map(recipe => (
+        <li class="recipe-element" key={recipe.id}>
+            <div class="recipe-header">
+                <h2>{recipe.Title}</h2>
+                <a class="bookmark-button"><BookmarkIcon /></a>
+            </div>
+            <RateElement rates={recipe.Rates}/>
+            <img class="recipe-img" src={recipe.Picture} alt="picture of food" />
+        </li>
+        ))}
+    </ul>
+    );
+}
+/*const RecipeListApp = (
+    <div id="content">
+        <h1>Recipes/Bookmarks</h1>
+        <RecipeList />
+    </div>
+);*/
+
+const RateElement = (props) => {
+    function createStarIcons(count) {
+        let stars = [];
+        console.log("createStarIcons: " + count)
+        for(let i = 0; i< count;i++){
+            stars.push(<StarIcon key={i}/>);
+        }
+        return (stars);
+    }
+    function createStarBorderIcons(count) {
+        console.log("createStarBorderIcons: " + count)
+        let stars = [];
+        for(let i = 0; i< count;i++){
+            stars.push(<StarBorderIcon key={i}/>);
+        }
+        return (stars);
+    }
+    function showRate() {
+        var currentRate = 0;
+        console.log(props.rates)
+        for(var i = 0; i < props.rates.length; i++) {
+        currentRate += props.rates[i].Rate;
+        if(props.rates[i].User == "ThisUser"){
+            return (
+            <a>
+                <span>
+                {createStarIcons(props.rates[i].User.Rate)}
+                </span>
+                {createStarBorderIcons(5 - props.rates[i].User.Rate)}
+            </a>
+            );
+        }
+        }
+        currentRate /= props.rates.length;
+        return (
+        <a>
+            {createStarIcons(Math.round(currentRate))}
+            {createStarBorderIcons(5 - Math.round(currentRate))}
+        </a>
+        );
+    }
+    
+    return (
+        <div class="rate">
+            {showRate()}
+        </div>
+    );
+}
+
+export default RecipeListApp;
