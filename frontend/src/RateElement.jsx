d4deba5140e665a45952b5509b8c87c539275317
Gideron <nico.alakonni@gmail.com>
2020-04-29 21:26:24 +0300

    Reduced duplicate code from RateElement

---
+++ frontend/src/RateElement.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+//material icons
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
+const RateElement = (props) => {
+    function createStarIcons(count) {
+        let stars = [];
+        console.log("createStarIcons: " + count)
+        for(let i = 0; i< count;i++){
+            stars.push(<StarIcon key={"StarIcon"+i}/>);
+        }
+        return (stars);
+    }
+    function createStarBorderIcons(count) {
+        console.log("createStarBorderIcons: " + count)
+        let stars = [];
+        for(let i = 0; i< count;i++){
+            stars.push(<StarBorderIcon key={"StarBorderIcon"+i}/>);
+        }
+        return (stars);
+    }
+    function showRate() {
+        var currentRate = 0;
+        console.log(props.rates)
+        for(var i = 0; i < props.rates.length; i++) {
+        currentRate += props.rates[i].Rate;
+        if(props.rates[i].User === "ThisUser"){
+            return (
+                <div className="rate">
+                    <span>
+                        {createStarIcons(props.rates[i].Rate)}
+                    </span>
+                    {createStarBorderIcons(5 - props.rates[i].Rate)}
+                </div>
+            );
+        }
+        }
+        currentRate /= props.rates.length;
+        return (
+            <div className="rate">
+                {createStarIcons(Math.round(currentRate))}
+                {createStarBorderIcons(5 - Math.round(currentRate))}
+            </div>
+        );
+    }
+    
+    return (
+        <div className="rate">
+            {showRate()}
+        </div>
+    );
+}
+
+export default RateElement;
--- frontend/src/RecipeListView.jsx
+++ frontend/src/RecipeListView.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 //material icons
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 //import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import StarIcon from '@material-ui/icons/Star';
-import StarBorderIcon from '@material-ui/icons/StarBorder';
 //mockup data
 import RecipeData from './mockup_data/RecipeList.json'
+import RateElement from './RateElement'
 
 import {Link} from "react-router-dom";
 
@@ -44,55 +43,6 @@ const RecipeList = () => {
         </Link>
         ))}
     </ul>
-    );
-}
-
-const RateElement = (props) => {
-    function createStarIcons(count) {
-        let stars = [];
-        console.log("createStarIcons: " + count)
-        for(let i = 0; i< count;i++){
-            stars.push(<StarIcon key={"StarIcon"+i}/>);
-        }
-        return (stars);
-    }
-    function createStarBorderIcons(count) {
-        console.log("createStarBorderIcons: " + count)
-        let stars = [];
-        for(let i = 0; i< count;i++){
-            stars.push(<StarBorderIcon key={"StarBorderIcon"+i}/>);
-        }
-        return (stars);
-    }
-    function showRate() {
-        var currentRate = 0;
-        console.log(props.rates)
-        for(var i = 0; i < props.rates.length; i++) {
-        currentRate += props.rates[i].Rate;
-        if(props.rates[i].User === "ThisUser"){
-            return (
-                <div className="rate">
-                    <span>
-                        {createStarIcons(props.rates[i].Rate)}
-                    </span>
-                    {createStarBorderIcons(5 - props.rates[i].Rate)}
-                </div>
-            );
-        }
-        }
-        currentRate /= props.rates.length;
-        return (
-            <div className="rate">
-                {createStarIcons(Math.round(currentRate))}
-                {createStarBorderIcons(5 - Math.round(currentRate))}
-            </div>
-        );
-    }
-    
-    return (
-        <div className="rate">
-            {showRate()}
-        </div>
     );
 }
 
