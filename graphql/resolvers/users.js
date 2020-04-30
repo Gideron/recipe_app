9eeb9a6edecc70667d9f6ce75a3279d622df10bd
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-04-30 15:14:13 +0300

    validators file and login

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,17 +1,80 @@
 const bc = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
 require("dotenv").config();
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
 const User = require("../../models/user");
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bc.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong credentials";
+        throw new UserInputError("Wrong credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { email, username, password, confirmPassword } },
       context,
       info
     ) {
+      const { valid, errors } = validateRegisterInput(
+        email,
+        username,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
       password = await bc.hash(password, 12);
 
       const newUser = new User({
@@ -22,15 +85,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        process.env.SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -18,5 +18,6 @@ module.exports = gql`
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
+    login(username: String!, password: String!): User!
   }
 `;
