8187f971d8cec59684c36337c7b62df551ee8298
Gideron <nico.alakonni@gmail.com>
2020-05-01 20:39:13 +0300

    Menu closes after use

--- frontend/src/NavigationElement.jsx
+++ frontend/src/NavigationElement.jsx
@@ -19,20 +19,25 @@ const NavigationBar = () => {
             menuOpen = false;
         }
     }
+    function closeMenu() {
+        const menu = document.getElementById('menu-window');
+        menu.classList.add("menu-closed");
+        menuOpen = false;
+    }
     return ([
         <nav key="navkey">
             <button id="menu-button" onClick={toggleMenu.bind(this)}>
                 {menuOpen ? <MenuCloseIcon /> : <MenuIcon />}
             </button>
-            <Link id="profile-button" to="profile"><PersonIcon /></Link>
+            <Link id="profile-button" to="profile" onClick={closeMenu.bind(this)}><PersonIcon /></Link>
         </nav>,
         <div id="menu-window" className="menu-window menu-closed" key="menukey">
-            <Link to="/">Home</Link>
-            <Link to="/recipes/Starters">Starters</Link>
-            <Link to="/recipes/Main dishes">Main dishes</Link>
-            <Link to="/recipes/Desserts">Desserts</Link>
+            <Link to="/" onClick={closeMenu.bind(this)}>Home</Link>
+            <Link to="/recipes/Starters" onClick={closeMenu.bind(this)}>Starters</Link>
+            <Link to="/recipes/Main dishes" onClick={closeMenu.bind(this)}>Main dishes</Link>
+            <Link to="/recipes/Desserts" onClick={closeMenu.bind(this)}>Desserts</Link>
         </div>
     ]);
-  }
+}
 
   export default NavigationBar;
