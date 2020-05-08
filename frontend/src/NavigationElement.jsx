b10037b99d2ec10fd6b501483e2c799d14a98494
TinoKapanen <32290202+TinoKapanen@users.noreply.github.com>
2020-05-08 20:56:49 +0300

    menu links to create category/recipe

--- frontend/src/NavigationElement.jsx
+++ frontend/src/NavigationElement.jsx
@@ -69,6 +69,10 @@ const NavigationBar = () => {
       ) : (
         <p>No categories</p>
       )}
+      <div id="menu-bottom">
+        <Link to="/create/category">Create Category</Link>
+        <Link to="/create/recipe">Create Recipe</Link>
+      </div>
     </div>,
   ];
 };
